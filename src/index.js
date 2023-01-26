1fa87022edff97c5fbf4258db3e1fb5f8ec95f45
andreiggr <andrei.ggr@gmail.com>
2023-01-26 16:05:46 +0200

    have themeColors provided by the widget

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import { ThemePickerWidget } from './Widgets';
 const applyConfig = (config) => {
   config.widgets.widget.theme_picker = ThemePickerWidget;
 
+  config.settings.themeColors = [
+    { value: 'default', title: 'Default' },
+    { value: 'primary', title: 'Primary' },
+    { value: 'secondary', title: 'Secondary' },
+    { value: 'tertiary', title: 'Tertiary' },
+  ];
+
   return config;
 };
 
