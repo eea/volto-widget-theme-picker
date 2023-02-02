baa5b83e7c74abc6d156f3b7ce6bf30b58d1895d
ichim-david <ichim.david@gmail.com>
2023-02-02 22:30:27 +0200

    removed default theme in favor of no theme value

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const applyConfig = (config) => {
   config.widgets.widget.theme_picker = ThemePickerWidget;
 
   config.settings.themeColors = [
-    { value: 'default', title: 'Default' },
+    { value: undefined, title: 'No theme' },
     { value: 'primary', title: 'Primary' },
     { value: 'secondary', title: 'Secondary' },
     { value: 'tertiary', title: 'Tertiary' },
